9b3495d8f1cd91bd5f427fdf0e8c84db7f5b95cd
sanakannn <sanakan.ver3@gmail.com>
2022-07-28 23:03:59 +0900

    corpusの整形とlabelの整形を追加 train_test_splitの引数の改善

--- train.py
+++ train.py
@@ -8,19 +8,35 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-df3 = pd.read_csv("C:\\Users\\p-user\\Desktop\\pythonProject1\\testdata\\test_corpus.tsv",
-                  sep=",", encoding="cp932")
-df = pd.read_csv("C:\\Users\\p-user\\Desktop\\pythonProject1\\testdata\\text.tsv")
-# dfに列名を付ける
+# df3 = pd.read_csv("C:\\Users\\p-user\\Desktop\\pythonProject1\\testdata\\test_corpus.tsv",  sep=",", encoding="cp932")
+df = pd.read_csv("C:\\Users\\hiroyuki\\Desktop\\same\\copy\\corpus.tsv", sep="\t")
+
 df.columns = ["date", "id", "text", "label"]
 df = df * 1
 text = df["text"]
-df = df.sample(frac=1, random_state=1)  # シャッフル
-grouped = df.groupby("label")
-df = grouped.head(n=4000)
+#corpusの整形
+text = text.str.replace("[", "")
+text = text.str.replace("]", "")
+
+print(df["label"])
+
+#labelの整形
+for i in range(len(df["label"])):
+    if df["label"].iloc[i] == -1:
+        df["label"][i] = 0
+print(df["label"])
+print(df["text"])
+
+# シャッフル
+df = df.sample(frac=1, random_state=1)
+# grouped = df.groupby("label")
+# df = grouped.head(n=4000)
+X = df["text"].values
+Y = df["label"].values
 
 
-x_train, x_test, y_train, y_test = train_test_split(df["text"], df["label"],
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y,
                                                     test_size=0.2, random_state=1)
 
 print(np.shape(x_train), np.shape(x_test))
