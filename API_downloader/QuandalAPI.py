e8df2ad0a0e8192d7ba58bfcb1cd38a0c01176fb
sanakannn <sanakan.ver3@gmail.com>
2022-07-30 22:43:26 +0900

    まだ

--- API_downloader/QuandalAPI.py
+++ API_downloader/QuandalAPI.py
@@ -4,15 +4,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+
+def get_data(code, start_date, end_date):
+    try:
+
+        data = quandl.get(code,date = { "gte": f"start_date", "lte": f"end_date" })
+        sleep_time = np.random.randint(6, 15)
+        time.sleep(sleep_time)
+        print(data)
+    except:
+        print(f"{code} is not found")
+        data = None
+        pass
+
+    return data
+
+
 quandl.ApiConfig.api_key = "jtfUsP_kX1yTxwJzcBMZ"
 data_list = pd.read_csv("C:\\Users\\hiroyuki\\Desktop\\same\copy\\train_test_data\\test_corpus.tsv", sep="\t")
 data_list_code = data_list["code"]
+
 for i in range(len(data_list_code)):
-    get_data = "TES/" + str(data_list_code[i])
-    sleep_time = np.random.randint(6, 15)
-    quandl_data = quandl.get(get_data, start_date="2019-01-01", end_date="2019-12-31")
-    time.sleep(sleep_time)
-    # グラフを描画
-    plt.plot(quandl_data["Close"])
-    plt.show()
+    code = "TSE/" + str(data_list_code[i])
+    start_date = "2019-01-01"
+    end_date = "2020-01-01"
+    print(code, start_date, end_date)
+
+    get_data = get_data(code, start_date, end_date)
+    get_data.to_csv("C:\\Users\\hiroyuki\\Desktop\\same\copy\\train_test_data\\test_corpus_data.tsv", sep="\t")
+"""
+    if get_data is not None:
+        data_list["data"][i] = get_data
+        print(f"{code} is downloaded")
+    else:
+        print(f"{code} is not downloaded")
+        data_list["data"][i] = None
+        pass
+"""
 
