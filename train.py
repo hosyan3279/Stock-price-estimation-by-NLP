f0b99f5441d06176330849c54df36d10a6f5cf0e
sanakannn <sanakan.ver3@gmail.com>
2022-08-05 22:05:15 +0900

    特徴量のエラーを修正

--- train.py
+++ train.py
@@ -25,29 +25,14 @@ for i in range(len(df["label"])):
     if df["label"].iloc[i] == -1:
         df["label"][i] = 0
 
-#print(df["label"])
-#print(df["text"])
-
 # シャッフル
 df = df.sample(frac=1, random_state=1)
-# grouped = df.groupby("label")
-# df = grouped.head(n=4000)
+
 X = df["text"].values
 Y = df["label"].values
 
-""""
-manual_data = pd.DataFrame(X,Y,columns=["text","label"])
-print(manual_data)
-manual_data.to_csv("C:\\Users\\hiroyuki\\Desktop\\same\\copy\\train_test_data\\corpus.tsv", sep="\t")
-"""
-"""
-print("x_train x_test \n", np.shape(x_train), np.shape(x_test))
-print("y_train y_test \n", np.shape(y_train), np.shape(y_test))
 
-print("x\n",x_train, x_test)
-print("y\n",y_train, y_test)
-"""
-
+# べくとらいざー
 lowercase = False
 tokenize = None
 preprocessor = None
@@ -56,11 +41,11 @@ vectorizer = TfidfVectorizer(max_features=100)
 
 X = vectorizer.fit_transform(X)
 
-#x_train_vec = vectorizer.fit_transform(x_train)
-#x_test_vec = vectorizer.fit_transform(x_test)
+# x_train_vec = vectorizer.fit_transform(x_train)
+# x_test_vec = vectorizer.fit_transform(x_test)
 
 x_train_vec, x_test_vec, y_train, y_test = train_test_split(X, Y,
-                                                    test_size=0.2, random_state=1)
+                                                            test_size=0.2, random_state=1)
 
 x_train_vec = np.nan_to_num(np.array(x_train_vec.todense()))
 x_test_vec = np.nan_to_num(np.array(x_test_vec.todense()))
@@ -71,12 +56,6 @@ print("  shape  \nx_train_vec x_test_vec \n", np.shape(x_train_vec), np.shape(x_
 print("  shape  \ny_train y_test \n", np.shape(y_train), np.shape(y_test))
 print("x_train_vec x_test_vec \n", x_train_vec, x_test_vec)
 print("y_train y_test \n", y_train, y_test)
-"""
-x_train_vec = random.sample(x_train_vec, 100)
-x_test_vec = random.sample(x_test_vec, 20)
-y_train = random.sample(y_train, 100)
-y_test = random.sample(y_test, 20)
-"""
 
 print("  shape  \nx_train_vec x_test_vec \n", np.shape(x_train_vec), np.shape(x_test_vec))
 print("  shape  \ny_train y_test \n", np.shape(y_train), np.shape(y_test))
@@ -89,4 +68,4 @@ clf.fit(x_train_vec, y_train)
 # clf.fit(x_train_vec, y_train)
 y_predict = clf.predict(x_test_vec)
 score = accuracy_score(y_test, y_predict)
-print("accuracy_score "+"{:.2f}".format(score))
+print("accuracy_score " + "{:.2f}".format(score))
